a236243a9369211fb808b6feb7315a6cdce891bc
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-24 08:12:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,18 +6,18 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  region  = "us-east-1"
-  # access_key = "my-access-key"
+                                         # (part 10-15) Authentication and Configuration
+provider "aws" {                         # Specifies that Terraform will use the AWS provider.
+  region  = "us-east-1"                  # Specifies which AWS region the created resources will be located in
+  # access_key = "my-access-key"         # HashiCorp recommends that you 'never' hard-code credentials into `*.tf configuration files`.
   # secret_key = "my-secret-key"
-  ## profile = "my-profile"
+  ## profile = "my-profile"              # Allows to use AWS credentials from the AWS CLI configuration. This is a more secure and flexible approach.
 }
-
-resource "aws_instance" "tf-ec2" {
-  ami           = "ami-022e1a32d3f742bd8"
-  instance_type = "t2.micro"
+                                             # (part 17-23) resource block
+resource "aws_instance" "tf-ec2" {           # Creates an EC2 instance resource named "tf-ec2".
+  ami           = "ami-022e1a32d3f742bd8"    # Specifies the Amazon Machine Image (AMI) code to use.
+  instance_type = "t2.micro"                 # It determines the type and size of the created instance.
   tags = {
-    "Name" = "created-by-tf"
+    "Name" = "created-by-tf"                 # Adds a "created-by-tf" tag to the EC2 instance.
   }
 }
