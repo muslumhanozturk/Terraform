0210339a9f178f50da9c16ce2b5464ef47abb91d
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-25 15:24:28 +0300

    Update variables.tf

--- terraform_5/variables.tf
+++ terraform_5/variables.tf
@@ -11,5 +11,11 @@ variable "ec2_ami" {
 }
 
 variable "s3_bucket_name" {
-  default = "mhan-s3-bucket-variable-addwhateveryouwant"
+#  default = "mhan-s3-bucket-variable-addwhateveryouwant"    
 }
+
+
+# If the default value of the variables is not entered or if they are commented out, when we enter the "terraform plan" and "terraform apply" command, it asks us to enter this value and we write the value.
+# If we don't want him to ask;
+# terraform plan -var="s3_bucket_name=mhan-new-s3-bucket"
+# terraform apply -var="s3_bucket_name=mhan-new-s3-bucket"
