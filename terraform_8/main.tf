a96e1d68a98936329c24696d6208c6a18e10fdf8
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-25 17:50:03 +0300

    Update main.tf

--- terraform_8/main.tf
+++ terraform_8/main.tf
@@ -22,4 +22,5 @@ variable "s3_bucket_name" {
 resource "aws_s3_bucket" "tf-s3" {
   bucket = "${var.s3_bucket_name}-${count.index}"     ### < ---
   count = var.num_of_buckets                          ### < ---
+# count = var.num_of_buckets != 0 ? var.num_of_buckets : 3      # conditional expressions
 }
