d54e92295ed36d699216019b55d20e2d2963cf8b
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-25 18:06:17 +0300

    Update main.tf

--- terraform_9/main.tf
+++ terraform_9/main.tf
@@ -1,38 +1,37 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "5.13.1"
     }
   }
 }
 
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
+variable "num_of_buckets" {
+  default = 0
 }
 
-resource "aws_instance" "tf-ec2" {
-  ami           = var.ec2_ami
-  instance_type = var.ec2_type
-  key_name      = "second-key-pair"
-  tags = {
-    "Name" = "${var.ec2_name}-instance"
-  }
+variable "s3_bucket_name" {
+  default     = "mhan-new-s3-bucket-addwhateveryouwant"
 }
 
 resource "aws_s3_bucket" "tf-s3" {
-  bucket = var.s3_bucket_name
+  # bucket = "${var.s3_bucket_name}-${count.index}"
+  # count = var.num_of_buckets
+  # count = var.num_of_buckets != 0 ? var.num_of_buckets : 1
+  for_each = toset(var.users)
+  bucket   = "example-tf-s3-bucket-${each.value}"
 }
 
-output "tf_example_public_ip" {
-  value = aws_instance.tf-ec2.public_ip
+resource "aws_iam_user" "new_users" {
+  for_each = toset(var.users)
+  name = each.value
 }
 
-output "tf_example_private_ip" {
-  value = aws_instance.tf-ec2.private_ip
-}
-
-output "tf-example-s3" {
-  value = aws_s3_bucket.tf-s3[*]
+output "uppercase_users" {
+  value = [for user in var.users : upper(user) if length(user) > 6]
 }
