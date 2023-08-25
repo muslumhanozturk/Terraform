48cd9ec04117c9c11b6ce20fbf9f052049cabdea
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-25 17:13:07 +0300

    Update main.tf

--- terraform_7/main.tf
+++ terraform_7/main.tf
@@ -1,23 +1,31 @@
-terraform {
-  required_providers {
-    aws = {                              # (part 1-9) When we create a resource, the "USE PROVIDER" code of that provider (for example, aws provider \
-      source = "hashicorp/aws"           # https://registry.terraform.io/providers/hashicorp/aws/latest/docs#authentication-and-configuration) \
-      version = "5.13.1"                 # is appended to the beginning of the .tf file. . This code loads provider.
-    }
+locals {                          # A local value assigns a name to an expression, so you can use it multiple times within a module without repeating it.
+    mytag = "mhan-local-name"
+}
+
+variable "ec2_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_ami" {
+  default = "ami-08a52ddb321b32a8c"
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_type
+  key_name      = "second-key-pair"
+  tags = {
+    Name = "${local.mytag}-come from locals"                  ###   < ---
   }
 }
-                                         # (part 10-15) Authentication and Configuration
-provider "aws" {                         # Specifies that Terraform will use the AWS provider.
-  region  = "us-east-1"                  # Specifies which AWS region the created resources will be located in
-  # access_key = "my-access-key"         # HashiCorp recommends that you 'never' hard-code credentials into `*.tf configuration files`.
-  # secret_key = "my-secret-key"
-  ## profile = "my-profile"              # Allows to use AWS credentials from the AWS CLI configuration. This is a more secure and flexible approach.
+
+variable "s3_bucket_name" {
+  default = "mhan-s3-bucket-variable-addwhateveryouwant"
 }
-                                             # (part 17-23) resource block
-resource "aws_instance" "tf-ec2" {           # Creates an EC2 instance resource named "tf-ec2".
-  ami           = "ami-022e1a32d3f742bd8"    # Specifies the Amazon Machine Image (AMI) code to use.
-  instance_type = "t2.micro"                 # It determines the type and size of the created instance.
+
+resource "aws_s3_bucket" "tf-s3" {
+  bucket = var.s3_bucket_name
   tags = {
-    "Name" = "created-by-tf"                 # Adds a "created-by-tf" tag to the EC2 instance.
+    Name = "${local.mytag}-come-from-locals"                  ###   < ---
   }
 }
