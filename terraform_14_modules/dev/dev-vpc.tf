4102593ac9452ce4506b7410662af4fddcb7ad8c
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-29 02:46:27 +0300

    Update dev-vpc.tf

--- terraform_14_modules/dev/dev-vpc.tf
+++ terraform_14_modules/dev/dev-vpc.tf
@@ -6,3 +6,9 @@ module "tf-vpc" {
 output "vpc-cidr-block" {
   value = module.tf-vpc.vpc_cidr
 }
+
+
+# When we do terraform plan & terraform apply,
+# it processes variable.tf and main.tf. 
+# but it does not process the outputs. 
+# should also be noted here.
