55e1dfa4dc31a9eebca845b083a13e403849bb98
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-28 12:19:07 +0300

    Update main.tf

--- terraform_10/main.tf
+++ terraform_10/main.tf
@@ -21,18 +21,18 @@ variable "ec2-type" {
     default = "t2.micro"
 }
 
-data "aws_ami" "tf_ami" {
-  most_recent = true
-  owners = ["self"]
+data "aws_ami" "tf_ami" {         # This line defines a new aws_ami datasource in the data block.This resource is used to query AWS AMIs and select those with certain characteristics.
+  most_recent = true              # This parameter enables the selection of the most recently created AMI. So it chooses the most recent one.
+  owners = ["self"]               # "self" here refers to your current account. i.e. it is used to import AMIs that you create yourself.
 
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
+  filter {                        # Filters are used to query AMIs based on the properties you want.
+    name = "virtualization-type"  # Wants to filter AMIs by virtualization type
+    values = ["hvm"]              # It will select AMIs with virtualization type "hvm" (Hardware Virtual Machine).
   }
 
   filter {
-    name = "architecture"
-    values = ["x86_64"]
+    name = "architecture"         # Used to query AMIs by architecture.
+    values = ["x86_64"]           # It will select AMIs with x86_64 architecture.
   }
   
 }
