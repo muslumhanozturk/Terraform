158f31232a6678881370f0a545bbb935d15e383a
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-29 02:27:54 +0300

    Update main.tf

--- terraform_15_import/main.tf
+++ terraform_15_import/main.tf
@@ -58,3 +58,9 @@ resource "aws_security_group" "tf-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+terraform init
+terraform import aws_security_group.tf-sg sg-01b92e29e828a2177         # If there is already a security group in the cloud, it is imported with this command. 
+terraform import "aws_instance.tf-instances[0]" i-090291cc33c16504c    # If the instance already exists in the cloud, it is imported with this command.
+terraform import "aws_instance.tf-instances[1]" i-092fe70d1cef163c1
