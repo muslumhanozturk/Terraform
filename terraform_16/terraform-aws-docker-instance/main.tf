2921bec423ea06c6ea83b3f4391b3a06f0a728fd
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-08-30 13:31:45 +0300

    Update main.tf

--- terraform_16/terraform-aws-docker-instance/main.tf
+++ terraform_16/terraform-aws-docker-instance/main.tf
@@ -29,7 +29,7 @@ data "aws_ami" "amazon-linux-2023" {
 }
 
 data "template_file" "userdata" {
-  template = file("${abspath(path.module)}/userdata.sh")
+  template = file("${abspath(path.module)}/userdata.sh")       #${abspath(path.module)} ifadesi, bu Terraform modülünün bulunduğu dizinin tam yolunu döndürür.
   vars = {
     myserver = var.server-name
   }
@@ -53,14 +53,14 @@ resource "aws_security_group" "tf-sec-gr" {
     Name = var.tag
   }
 
-  dynamic "ingress" {
-    for_each = var.docker-instance-ports
-    iterator = port
-    content {
-      from_port = port.value
+  dynamic "ingress" {                        # dynamic bloğu, belirli port numaralarını döngü ile alarak bu portlar için gelen trafiğe izin veren kuralları oluşturuyor.
+    for_each = var.docker-instance-ports     # döngüde kullanılacak olan elemanları belirler.
+    iterator = port                          # her döngü adımında kullanılacak olan iterator (döngü değişkeni) adını belirtir. burada port döngü değişkeni
+    content {                                # içerik bloğu, her bir port numarası için gelen trafiğe izin veren bir güvenlik grubu kuralı oluşturur.
+      from_port = port.value                 # her adımda farklı bir port numarası kullanılır.
       to_port = port.value
       protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ["0.0.0.0/0"]            # tüm adreslerden gelen trafiği kabul edecek.
     }
   }
 
