8fc5d69ee08871dc6421dedb7ddc22c621fdcbf0
M.Han <muslumhanozturk@gmail.com>
2023-09-01 19:16:57 +0300

    new

---
+++ terraform_18/project/tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "muslumhanozturk"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "second-key-pair"
+}
+
+variable "hosted-zone" {
+  default = "muslumhanozturk.com"
+}
